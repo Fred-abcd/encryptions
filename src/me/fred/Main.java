0d4fe51e7fed0a26ec5b51c3324410fa553866d2
CookieClientDEV <jro.myp@gmail.com>
2022-12-27 15:29:14 +0100

    oops

--- src/me/fred/Main.java
+++ src/me/fred/Main.java
@@ -8,13 +8,13 @@ public class Main {
 
     public static void main(String[] args) {
         //Ceasar
-        System.out.println("Ceasar (5): " + Caesar.caesarEncrypt("TheOrandeDev on Github", 5));;
+        System.out.println("Ceasar (5): " + Caesar.caesarEncrypt("TheOrangeDev on Github", 5));;
 
         //Xor
-        System.out.println("Xor (5 (25)): " + Xor.xorEncrypt("TheOrandeDev on Github", 5));
+        System.out.println("Xor (5 (25)): " + Xor.xorEncrypt("TheOrangeDev on Github", 5));
 
         //Morse
-        System.out.println("Morse: " + Morse.morseEncrypt("TheOrandeDev on Github"));
+        System.out.println("Morse: " + Morse.morseEncrypt("TheOrangeDev on Github"));
 
 
     }
